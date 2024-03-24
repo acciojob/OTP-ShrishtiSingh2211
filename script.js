ecb8b76746cf13bb16bd443c86f12f854a058692
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-24 22:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //your JS code here. If required.
-let inputs = Array.from(document.getElementsByClassName('code')); // Get all input fields
-inputs.forEach((input, index) => {
-  input.addEventListener('input', () => {  // Add input event listener
-    if (input.value) {  // If a digit is entered
-      if (index < inputs.length - 1) {  // If it's not the last input field
-        inputs[index + 1].focus();  // Focus on the next input field
+let inputs = Array.from(document.querySelectorAll('.code'));
+
+inputs.forEach((input, i) => {
+  input.addEventListener('input', () => {
+    if(input.value.length === 1) {
+      if(inputs[i+1]) {
+        inputs[i+1].focus();
       }
     }
   });
-  input.addEventListener('keydown', (event) => {  // Add keydown event listener
-    if (event.key === "Backspace") {  // If backspace is pressed
-      if (index > 0) {  // If it's not the first input field
-        inputs[index - 1].focus();  // Focus on the previous input field
-      }
+
+  input.addEventListener('keydown', (event) => {
+    if(event.key === "Backspace" && i > 0 && input.value.length === 0) {
+      inputs[i-1].focus();
     }
   });
 });
